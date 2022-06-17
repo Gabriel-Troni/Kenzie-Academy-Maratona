449765bcd17a47a39a734dccc374b9873ef4eaa3
Gabriel-Troni <troni.gabriel.10@gmail.com>
2022-06-17 16:06:01 -0400

    HTML-e-CSS

---
+++ assets/script/database.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: "01",
+        title: "Kenzie Burgão",
+        description: "Parágrafo descrevendo o prato",
+        price: "28,00",
+        img: "./assets/img/kenzie_burgao.png",
+    },
+    {
+        id: "02",
+        title: "Refrigerante",
+        description: "Parágrafo descrevendo o prato",
+        price: "7,50",
+        img: "./assets/img/refrigerante.png",
+    },
+    {
+        id: "03",
+        title: "Kenzie Burguinho",
+        description: "Parágrafo descrevendo o prato",
+        price: "22,00",
+        img: "./assets/img/kenzie_burguinho.png",
+    },
+    {
+        id: "04",
+        title: "Frango frito",
+        description: "Parágrafo descrevendo o prato",
+        price: "25,00",
+        img: "./assets/img/frango_frito.png",
+    },
+    {
+        id: "05",
+        title: "Batata frita",
+        description: "Parágrafo descrevendo o prato",
+        price: "15,00",
+        img: "./assets/img/batata_frita.png",
+    },
+    {
+        id: "06",
+        title: "Sanduíche de frango frito",
+        description: "Parágrafo descrevendo o prato",
+        price: "20,00",
+        img: "./assets/img/sanduiche_frango_frito.png",
+    },
+];
